424400b0264bf2097478a6136a4e034763ad0300
ludwiga5 <aludwig5@outlook.com>
2026-03-01 20:04:39 -0500

    Baseline for HW

---
+++ CS240/Homeworks/GHP3/ghp3.cpp
@@ -0,0 +1,27 @@
+/*
+Purpose: PUT HW DESCRIPTION HERE
+
+Written by Alex Ludwig
+March 2026
+Language: C++
+Compiler: g++ Apple clang version 17.0.0 (clang-1700.4.4.1)
+*/
+
+
+//$(found letter)-'a' returns ascii value
+
+
+#include <iostream>
+#include <fstream> //file readin functions
+#include <string>
+#include <cassert>
+#include <cctype> //toLower() & toUpper()
+
+using namespace std;
+
+int main(void){
+
+
+
+
+}
